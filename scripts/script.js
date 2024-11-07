43066c8898309181ec2494d4871108841076952b
wellington <wellingtoncarlos.tech@gmail.com>
2024-11-06 22:09:47 -0300

    inserido a logica para calcular os números

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,23 @@ function insert(num){
     let numero = document.querySelector('.resultado').innerHTML
     document.querySelector('.resultado').innerHTML = numero + num
 }
+
+function clean(){
+    document.querySelector('.resultado').innerHTML = ''
+}
+
+function back(){
+    let resultado = document.querySelector('.resultado').innerHTML
+    document.querySelector('.resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+function calcular(){
+    let resultado = document.querySelector('.resultado').innerHTML
+
+    if(resultado){
+        document.querySelector('.resultado').innerHTML = eval(resultado)
+    }else{
+        document.querySelector('.resultado').innerHTML = 'error'
+        document.querySelector('.resultado').style.textAlign = 'center'
+    }
+}
